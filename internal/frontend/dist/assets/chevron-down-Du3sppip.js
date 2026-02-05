aa288605041d6d7397b3cf907a756ec296efe2ae
Reynaldo Endis Peña <reynaldo.endis@gmail.com>
2026-02-04 22:28:59 -0300

    fix: Correct asset paths in index.html from absolute to relative and update build output filenames.

--- internal/frontend/dist/assets/chevron-down-Du3sppip.js
+++
@@ -1,6 +0,0 @@
-import{ae as o}from"./index-DwNOOeRl.js";/**
- * @license lucide-react v0.513.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const e=[["path",{d:"m6 9 6 6 6-6",key:"qrunsl"}]],c=o("chevron-down",e);export{c as C};
--- internal/frontend/dist/assets/templates-CuTu2Ul_.js
+++
@@ -1,1 +0,0 @@
-import{j as t,O as o}from"./index-DwNOOeRl.js";function s(){return t.jsx(o,{})}export{s as component};
